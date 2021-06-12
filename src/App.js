4721f5a4efc9e116a25e49d0435c3eb355cf9411
yaducse <yadubniketham@gmail.com>
2021-06-13 02:35:50 +0530

    tried adding the basic setup of create-react-app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import './style.css';
+import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from './Header';
 import Home from './Home';
